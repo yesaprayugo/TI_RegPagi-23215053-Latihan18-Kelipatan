401d2b342b7102401ac3620135e78e5d31a858a8
yesaprayugo <yesaprayugo4@gmail.com>
2024-11-04 09:56:00 +0700

    Add files via upload

---
+++ Kelipatan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan5;
+
+/**
+ *
+ * @author Dell
+ */
+public class Kelipatan {
+    public static void main(String[] args) {
+        double nilaiAwal = 3.5;
+        double batasAtas = 35.0;
+
+        // Looping untuk mencetak kelipatan 3.5
+        for (double i = nilaiAwal; i <= batasAtas; i += nilaiAwal) {
+            System.out.println(i);
+        }
+    }
+}
